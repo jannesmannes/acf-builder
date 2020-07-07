f300e43e7fa73fbf133afa1bc61580e136637864
Jan Henkes <jan@jannesmannes.nl>
2020-07-07 09:23:23 +0200

    Quickfix: do not transform field keys

--- src/Transform/NamespaceFieldKey.php
+++ src/Transform/NamespaceFieldKey.php
@@ -34,6 +34,7 @@ class NamespaceFieldKey extends RecursiveTransform
      */
     public function transformValue($value)
     {
+        return $value;
         $namespace = 'field_';
         $groupName = $this->getBuilder()->getName();
 
